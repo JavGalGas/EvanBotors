f271288395c4df56c1cf4152d91c21e34f9ae6cd
EvanBotors <145196986+EvanBotors@users.noreply.github.com>
2024-01-12 16:56:28 +0100

    copia 8

---
+++ copiaDAM/ED/Banco/Banco/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Client
+    {
+        private string _name;
+        private int _id;
+        private string _direccion;
+        private int _edad;
+
+        private List<Account> _cuentas = new List<Account>();
+
+        public Client(string name, int id, string direccion, int edad)
+        {
+            _name = name;
+            _id = id;
+            _direccion = direccion;
+            _edad = edad;
+        }
+
+        public void CreateCuenta(long id)
+        {
+            Account cuenta = new Account();
+            _cuentas.Add(cuenta);
+        }
+        public static Client CargarCliente(string name)
+        {
+            Client c=new Client();
+            //...
+            return c;
+        }
+
+    }
+}
---
+++ copiaDAM/ED/Banco/Banco/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+        }
+    }
+}
