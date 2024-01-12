134a8b98466ff998cb29f660e75d5fbea0595565
EvanBotors <145196986+EvanBotors@users.noreply.github.com>
2024-01-12 16:58:06 +0100

    copia 9

---
+++ copiaDAM/ED/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public static int  SumatorioRec(int n)
+        {
+            if (n <= 0)
+                return 0;
+            return n + SumatorioRec(n - 1);
+        }
+    }
+}
---
+++ copiaDAM/ED/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Class1.SumatorioRec(4);
+            Console.WriteLine(num1);
+        }
+    }
+}
